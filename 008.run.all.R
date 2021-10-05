96ee9f2a669928e2cf7279418e1351cc6d744605
emmamk <michitsuruta7@gmail.com>
2021-10-05 07:17:31 -0700

    changed file name (006.gometh.kegg* -> 007.gometh.kegg*)

--- 008.run.all.R
+++ 008.run.all.R
@@ -7,5 +7,5 @@ source("003.quality.control.R")
 source("004.normalizations.R")
 source("005.differential.methylated.analysis.R")
 source("006.gometh.go.overlaps.exclusives.R")
-source("006.gometh.kegg.sig.overlaps.exclusives.R")
+source("007.gometh.kegg.sig.overlaps.exclusives.R")
 
